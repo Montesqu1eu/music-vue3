142c3339bb4f6dc6ccc9fe3ba9cc729f20a41fdf
Ildar Shamsetdinov <ildar.golden@gmail.com>
2023-12-28 16:46:12 +0700

    routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,51 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import useUserStore from '@/stores/user';
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: () => import('@/views/HomePage.vue'),
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: () => import('@/views/AboutPage.vue'),
+  },
+  {
+    name: 'manage',
+    path: '/manage',
+    component: () => import('@/views/ManagePage.vue'),
+    beforeEnter: (to, from, next) => {
+      console.log('Manage guard');
+
+      next();
+    },
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
-  ],
+  routes,
+  linkExactActiveClass: 'text-yellow-500',
+});
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiresAuth) {
+    next();
+    return;
+  }
+
+  const store = useUserStore();
+
+  store.userLoggedIn ? next() : next({ name: 'home' });
 });
 
 export default router;
